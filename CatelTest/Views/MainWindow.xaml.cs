6bdce67706d93d1b152dee7051d2a040011b0f24
Maxim Kopylov <87maxi@gmail.com>
2019-02-02 20:00:15 +0300

    Initial commit

---
+++ CatelTest/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleTest.Views
+{
+  using Catel.Windows;
+
+  public partial class MainWindow
+  {
+    public MainWindow() : base(DataWindowMode.Custom, focusFirstControl:false)
+    {
+      InitializeComponent();
+    }
+  }
+}
